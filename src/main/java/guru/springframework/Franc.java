23902b1cd6ae988a3f347a269711770c92ee4fb8
Christian Preß <Christian.Press@syracos.com>
2020-04-16 00:10:42 +0200

    Times We're Livin' In

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -1,12 +1,17 @@
 package guru.springframework;
 
 public class Franc extends Money {
-    public Franc(int amount) {
-        this.amount = amount;
+    public Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
-    public Franc times(int multiplier) {
-        return new Franc(amount * multiplier);
+    @Override
+    public String currency() {
+        return currency;
+    }
+
+    public Money times(int multiplier) {
+        return Money.franc(amount * multiplier);
     }
 
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -2,13 +2,23 @@ package guru.springframework;
 
 public abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public String currency() {
+        return currency;
+    }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public abstract Money times(int multiplier);
