94dd6ac7d1e0a9b7eace182a055b1501e043ea41
Christian Preß <Christian.Press@syracos.com>
2020-04-15 23:58:05 +0200

    Makin' Objects

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,7 +1,17 @@
 package guru.springframework;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object value) {
